ea7813b3c7ddd2b993d0af65659d5958799d4324
Marco Petri <marco.petri@mail.polimi.it>
2022-05-03 22:07:21 +0200

    ARIMA plot for search

--- AnomalyDetection/RunHyperparameter.py
+++ AnomalyDetection/RunHyperparameter.py
@@ -1,17 +1,15 @@
 from typing import Tuple
 
+import matplotlib
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.preprocessing import StandardScaler
 from skopt.space import Integer, Categorical
 
-from models.time_series.anomaly import TimeSeriesAnomalyLOF
 from models.time_series.anomaly.TimeSeriesAnomalyARIMA import \
 	TimeSeriesAnomalyARIMA
 from reader.NABTimeSeriesReader import NABTimeSeriesReader
-from tuning.hyperparameter.GaussianProcessesSearch import GaussianProcessesSearch
 
 # DATASET 1: ambient_temperature_system_failure
 # DATASET 2: nyc_taxi
@@ -21,8 +19,8 @@ DATASET_PATH = "data/dataset/"
 DATASET = "ambient_temperature_system_failure.csv"
 PURE_DATA_KEY = "realKnownCause/ambient_temperature_system_failure.csv"
 GROUND_WINDOWS_PATH = "data/dataset/combined_windows.json"
-TRAIN = True
-LOAD_PREVIOUS = False
+TRAIN = False
+LOAD_PREVIOUS = True
 
 def preprocess(X) -> np.ndarray:
 	return StandardScaler().fit_transform(X)
@@ -36,15 +34,16 @@ data_labels = training["target"]
 dataframe = training.copy()
 dataframe["value"] = data
 
-# 0 = AR, 1 = MA, 2 = ARMA
+# 0 = AR, 1 = MA, 2 = ARIMA
 def get_orders(type: int = 0) -> list:
 	history = results.get_history()
 	if type == 0:
 		orders = [x[1][0] for x in history[1::]]
-	else:
+	elif type == 1:
 		orders = [x[1][2] for x in history[1::]]
+	else:
+		orders = [x[1] for x in history[1::]]
 	return orders
-
 
 def get_scores() -> list:
 	history = results.get_history()
@@ -61,8 +60,45 @@ def plot_AR_MA_score(order, score, fig_size: Tuple = (16, 6)):
 	plt.title("AR/MA search")
 	plt.show()
 	
-def plot_ARMA_score(order, score, fig_ratio):
-	pass
+def create_ARIMA(ar: list, diff: list, ma: list) -> list:
+	configs = []
+	
+	for ar_o in ar:
+		for diff_o in diff:
+			for ma_o in ma:
+				configs.append((ar_o, diff_o, ma_o))
+				
+	return configs
+
+def plot_ARMA_score(order, score, fig_ratio: float = 0.5, max_score: float = 1000.0):
+	matplotlib.use('Qt5Agg')
+	fig = plt.figure(figsize=plt.figaspect(fig_ratio))
+	ax = fig.add_subplot(projection='3d')
+	x, y, z = [], [], []
+
+	for i in range(len(order)):
+		x.append(order[i][0])
+		y.append(order[i][2])
+		z.append(score[i])
+
+	x, y, z = np.array(x), np.array(y), np.array(z)
+	
+	# eliminate points over the maximum score
+	correct_points = np.argwhere(z < max_score)
+	wrong_points = np.argwhere(z >= max_score)
+	xw, yw, zw = x[wrong_points], y[wrong_points], z[wrong_points]
+	zw[:] = -1
+	x, y, z = x[correct_points], y[correct_points], z[correct_points]
+	
+	x, y, z = x.reshape((-1, 1)), y.reshape((-1, 1)), z.reshape((-1, 1))
+	xw, yw, zw = xw.reshape((-1, 1)), yw.reshape((-1, 1)), zw.reshape((-1, 1))
+	
+	ax.scatter3D(x, y, z)
+	ax.scatter3D(xw, yw, zw, c="r")
+	ax.set_xlabel('AR order', fontweight='bold')
+	ax.set_ylabel('MA order', fontweight='bold')
+	ax.set_zlabel('BIC', fontweight='bold')
+	plt.show()
 
 def evaluate_time_series(train_data: np.ndarray,
 						 train_labels: np.ndarray,
@@ -80,18 +116,12 @@ hyper_searcher = TimeSeriesGridSearch([
 										  # Real(0.0, 1.0, name="anomaly_threshold"),
 										  # Real(0.00001, 0.5, prior="log-uniform", name="contamination"),
 										  #Integer(2, 100, name="n_estimators"),
-										  Categorical([(0, 1, 1),
-													   (0, 1, 5),
-													   (0, 1, 10),
-													   (0, 1, 15),
-													   (0, 1, 20),
-													   (0, 1, 25),
-													   (0, 1, 30),
-													   (0, 1, 35),
-													   (0, 1, 40)], name="order")
+										  Categorical(create_ARIMA([1, 5, 10, 15, 20, 25, 30, 35, 40],
+																   [1],
+																   [1, 5, 10, 15, 20, 25, 30, 35, 40]), name="order")
 									  ],
-									  "data/searches/ma/",
-									  "no_refit_gls_statespace_ma",
+									  "data/searches/arima/",
+									  "temp_no_refit_gls_statespace_arima",
 									  TimeSeriesSplit(n_splits=5),
 									  load_checkpoint=LOAD_PREVIOUS)
 if TRAIN:
@@ -100,9 +130,13 @@ if TRAIN:
 results = hyper_searcher.get_results()
 results.print_search()
 
-orders = get_orders(1)
+model = 2
+orders = get_orders(model)
 scores = get_scores()
-orders_idx = np.argsort(np.array(orders))
-orders = np.array(orders)[orders_idx]
-scores = np.array(scores)[orders_idx]
-plot_AR_MA_score(orders, scores)
+if model == 1 or model == 0:
+	orders_idx = np.argsort(np.array(orders))
+	orders = np.array(orders)[orders_idx]
+	scores = np.array(scores)[orders_idx]
+	plot_AR_MA_score(orders, scores)
+else:
+	plot_ARMA_score(orders, scores)
