e79df9680875ca57a29b54f0735108f6519bccc1
Marco Petri <marco.petri@mail.polimi.it>
2022-08-05 01:34:59 +0200

    Interface to get best configuration

--- AnomalyDetection/mleasy/tuning/hyperparameter/HyperparameterSearchResults.py
+++ AnomalyDetection/mleasy/tuning/hyperparameter/HyperparameterSearchResults.py
@@ -4,48 +4,74 @@ from mleasy.tuning.hyperparameter.IHyperparameterSearchResults import IHyperpara
 
 
 class HyperparameterSearchResults(IHyperparameterSearchResults):
-	"""It stores the results of a hyperparameter search.
-	
-	Parameters
-	----------
-	history : ndarray of shape (n_search + 1, params + 1)
-		It represents the history of the hyperparameter search. The first row is
-		the name of the parameters represented by the columns of the history.
-	"""
-	
-	def __init__(self, history: list):
-		super().__init__()
-		
-		self.history = history
-	
-	def get_best_score(self) -> float:
-		return np.min(self.history[1:, 0])
-	
-	def get_num_iterations(self) -> int:
-		return len(self.history) - 1
-	
-	def get_history(self) -> list:
-		return self.history.copy()
-	
-	def print_search(self, *args, **kwargs) -> None:
-		if len(self.history) == 0:
-			print("There is no search")
-		else:
-			scores = [x[0] for x in self.history[1::]]
-			tries = np.array(scores)
-			indices = (np.argsort(tries) + 1).tolist()
-			indices.reverse()
-			indices.insert(0, 0)
-			tries = [self.history[i] for i in indices]
-	
-			print("Total number of tries: ", len(tries) - 1)
-			first = True
-			for config in tries:
-				if first:
-					first = False
-				else:
-					text = ""
-					for i in range(len(config)):
-						text += str(tries[0][i])
-						text += ": " + str(config[i]) + " "
-					print(text)
+    """It stores the results of a hyperparameter search.
+    
+    Parameters
+    ----------
+    history : ndarray of shape (n_search + 1, params + 1)
+        It represents the history of the hyperparameter search. The first row is
+        the name of the parameters represented by the columns of the history.
+    """
+    
+    def __init__(self, history: list):
+        super().__init__()
+        
+        self.history = history
+    
+    def get_best_score(self) -> float:
+        if len(self.history) == 0:
+            print("There is no search")
+        else:
+            return np.min(self.history[1:, 0])
+    
+    def get_best_config(self) -> dict:
+        if len(self.history) == 0:
+            print("There is no search")
+        else:
+            tries = self._get_descending_score_tries()
+            
+            best_config = dict()
+            for i, key in enumerate(tries[0]):
+                best_config[key] = tries[-1][i]
+            
+            return best_config
+    
+    def get_num_iterations(self) -> int:
+        return len(self.history) - 1
+    
+    def get_history(self) -> list:
+        return self.history.copy()
+    
+    def print_search(self, *args, **kwargs) -> None:
+        if len(self.history) == 0:
+            print("There is no search")
+        else:
+            tries = self._get_descending_score_tries()
+            
+            print("Total number of tries: ", len(tries) - 1)
+            first = True
+            for config in tries:
+                if first:
+                    first = False
+                else:
+                    text = ""
+                    for i in range(len(config)):
+                        text += str(tries[0][i])
+                        text += ": " + str(config[i]) + " "
+                    print(text)
+    
+    def _get_descending_score_tries(self) -> list:
+        """Gets all the tries in descending order (worst top, best bottom).
+        
+        Returns
+        -------
+        all_tries
+            Tries ordered from worst to best.
+        """
+        scores = [x[0] for x in self.history[1::]]
+        tries = np.array(scores)
+        indices = (np.argsort(tries) + 1).tolist()
+        indices.reverse()
+        indices.insert(0, 0)
+        tries = [self.history[i] for i in indices]
+        return tries
--- AnomalyDetection/mleasy/tuning/hyperparameter/IHyperparameterSearchResults.py
+++ AnomalyDetection/mleasy/tuning/hyperparameter/IHyperparameterSearchResults.py
@@ -5,62 +5,73 @@ import numpy as np
 
 
 class IHyperparameterSearchResults(ABC):
-	"""Interface to represent hyperparameter search result exposed methods.
+    """Interface to represent hyperparameter search result exposed methods.
     """
-	
-	@abc.abstractmethod
-	def get_best_score(self) -> float:
-		"""Finds the best score obtained on the search and returns it.
-		
-		Returns
-		-------
-		best_score: float
-			The best score obtained on the search.
-		"""
-		pass
-	
-	@abc.abstractmethod
-	def get_num_iterations(self) -> int:
-		"""Returns the number of iterations passed searching.
-		
-		Returns
-		-------
-		num_iter : int
-			The number of iterations performed on search.
-		"""
-		pass
-	
-	@abc.abstractmethod
-	def get_history(self) -> np.ndarray:
-		"""Returns the history of the hyperparameter search.
-		
-		Returns
-		-------
-		history : ndarray of shape (n_iterations, 2)
-			The history of the search in which each element is composed of two
-			values: score and tried parameters as a dictionary.
-		"""
-		pass
-	
-	@abc.abstractmethod
-	def print_search(self, *args,
-					 **kwargs) -> None:
-		"""Prints the results of the search stored on the file path.
+    
+    @abc.abstractmethod
+    def get_best_score(self) -> float:
+        """Gets the best score obtained on the search.
+        
+        Returns
+        -------
+        best_score : float
+            The best score obtained on the search.
+        """
+        pass
+    
+    @abc.abstractmethod
+    def get_best_config(self) -> dict:
+        """Gets the best configuration obtained on the search.
+        
+        Returns
+        -------
+        best_config : dict
+            The best configuration of the search.
+        """
+        pass
+    
+    @abc.abstractmethod
+    def get_num_iterations(self) -> int:
+        """Returns the number of iterations passed searching.
+        
+        Returns
+        -------
+        num_iter : int
+            The number of iterations performed on search.
+        """
+        pass
+    
+    @abc.abstractmethod
+    def get_history(self) -> np.ndarray:
+        """Returns the history of the hyperparameter search.
+        
+        Returns
+        -------
+        history : ndarray of shape (n_iterations, 2)
+            The history of the search in which each element is composed of two
+            values: score and tried parameters as a dictionary.
+        """
+        pass
+    
+    @abc.abstractmethod
+    def print_search(self, *args,
+                     **kwargs) -> None:
+        """Prints the results of the search stored on the file path.
 
-		It uses the format specified by the fit function to read and print the
-		results of the search at the specified file path. If there is no search
-		file, an error will be raised.
+        It uses the format specified by the fit function to read and print the
+        results of the search at the specified file path. If there is no search
+        file, an error will be raised.
 
-		Parameters
-		----------
-		args
-			Not used, present to allow multiple inheritance and signature change.
+        Parameters
+        ----------
+        args
+            Not used, present to allow multiple inheritance and signature change.
 
-		kwargs
-			Not used, present to allow multiple inheritance and signature change.
+        kwargs
+            Not used, present to allow multiple inheritance and signature change.
 
-		Returns
-		-------
-		None
-		"""
-		pass
+        Returns
+        -------
+        None
+        """
+        pass
