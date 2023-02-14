258b83bb18960a57ee2560d9fb5a78432a236be6
Marco Petri <marco.petri@mail.polimi.it>
2023-02-14 10:08:09 +0100

    Remove old and no more used code

--- anomalearn/visualizer/__init__.py
+++ anomalearn/visualizer/__init__.py
@@ -1,18 +1,12 @@
-from .Viewer import get_bars_indices_on_test_df
-from .Viewer import get_windows_indices
-from .Viewer import plot_confusion_matrix
-from .Viewer import plot_precision_recall_curve
-from .Viewer import plot_roc_curve
-from .Viewer import plot_time_series_forecast
-from .Viewer import plot_time_series_ndarray
-from .Viewer import plot_time_series_with_predicitons_bars
-from .Viewer import plot_univariate_time_series
-from .Viewer import plot_univariate_time_series_predictions
 from .bar_plots import bar_class_distribution
 from .bar_plots import bar_plot
+
 from .box_plots import box_plot
+
 from .line_plots import confidence_line_plot
 from .line_plots import line_plot
+
 from .pie_plots import pie_class_distribution
 from .pie_plots import pie_plot
+
 from .scatter_plots import scatter_plot
