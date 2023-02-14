eef07916689f2d6e6701ae40c04e1f58b7210ef7
Marco Petri <marco.petri@mail.polimi.it>
2023-02-14 20:16:48 +0100

    Refactored analysis package and tests

--- anomalearn/analysis/DecompositionMethod.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class DecompositionMethod(Enum):
-    MOVING_AVERAGE = "MOVING_AVERAGE"
-    STL = "STL"
--- anomalearn/analysis/StationarityTest.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class StationarityTest(Enum):
-    ADFULLER = "ADFULLER",
-    KPSS = "KPSS"
